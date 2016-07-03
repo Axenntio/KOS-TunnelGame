8a97e806b2bc7c63d4a2632135ce872ffc28ad9b
Axenntio <axenntio@live.com>
2016-07-03 12:54:54 +0200

    Left Shifting by sector

--- main.c
+++ main.c
@@ -18,27 +18,24 @@ bool getPixel(SCREEN*, unsigned char, unsigned char);
 void main() {
 	SCREEN *screen;
 	uint8_t key=NULL, posY=22, height=16, space=24;
-	uint8_t i, j;
+	uint16_t i;
 	unsigned char _;
 	load_library("/lib/core");
 	get_lcd_lock();
 	get_keypad_lock();
 	screen = screen_allocate();
 	screen_clear(screen);
-	screen_draw(screen);
 	draw_rect_or(screen, height, 95, 0, 0);
 	draw_rect_or(screen, 64-(height+space), 95, height+space, 0);
 	draw_sprite_xor(screen, 0, posY, airplane_height, &airplane_sprite);
 	while (1) {
-		//screen_clear(screen);
 		draw_sprite_xor(screen, 0, posY, airplane_height, &airplane_sprite);
-		//draw_window(screen, "Axenntio's Test", WIN_DEFAULTS);
 		switch(get_random()%3){
 			case 1:
-				if(height>0) height--;
+				if(posY>0) posY--;
 				break;
 			case 2:
-				if(height<95) height++;
+				if(posY<95-space) posY++;
 				break;
 			default:
 				break;
@@ -54,31 +51,27 @@ void main() {
 			set_pixel(screen, 95, i);
 		}
 
+		//Left Shift
+		for(i=0;i<768;i++){
+			screen[i]=screen[i]<<1;
+		}
 
-		for(i=1;i<96;i++){
+		/*for(i=1;i<96;i++){
 			for(j=0;j<64;j++){
 				if(getPixel(screen, i, j)){set_pixel(screen, i-1, j);}
 				else{reset_pixel(screen, i-1, j);}
 			}
-		}
+		}*/
 
 		key = app_get_key(&_);
-		if(key==KEY_UP) {posY--;}
-		if(key==KEY_DOWN) {posY++;}
+		if(key==KEY_UP) {height--;}
+		if(key==KEY_DOWN) {height++;}
 		draw_sprite_xor(screen, 0, posY, airplane_height, &airplane_sprite);
 		screen_draw(screen);
 		//ksleep(10);
 	}
 }
 
-bool getPixel(SCREEN* data, unsigned char x, unsigned char y){
-	return getBit(getSector(data, x/8, y), 7-(x%8));
-}
-
-bool getBit(unsigned char byte, unsigned char number){
-	return (byte >> number) & 0x01;
-}
-
-unsigned char getSector(SCREEN* data, unsigned char x, unsigned char y){
-	return data[(y*12)+x];
-}
+bool getPixel(SCREEN* data, unsigned char x, unsigned char y){return getBit(getSector(data, x/8, y), 7-(x%8));}
+bool getBit(unsigned char byte, unsigned char number){return (byte >> number) & 0x01;}
+unsigned char getSector(SCREEN* data, unsigned char x, unsigned char y){return data[(y*12)+x];}
